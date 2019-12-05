3106d529d6a5641bb0596354d2dea3bff57be39e
Никита Лемешенко <n.lemeshenko@magdv.com>
2019-12-05 09:03:28 +0700

    Code cleaning

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ const resource = "/api/persons";
 app.use(cors());
 app.use(resource, personsRouter);
 
-app.get("/info", (req, res) => {
-  const countInfo = `<p>Phonebook has info for ${persons.length} people</p>`;
-  const reqTimeInfo = `<p>${new Date()}</p>`;
-  res.send(`${countInfo}${reqTimeInfo}`);
-});
-
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
