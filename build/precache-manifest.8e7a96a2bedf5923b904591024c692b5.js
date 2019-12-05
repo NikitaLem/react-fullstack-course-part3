9a0112875d89fad4528b6f7a916dd2155a1e283f
Никита Лемешенко <n.lemeshenko@magdv.com>
2019-12-05 09:56:34 +0700

    new front build

---
+++ build/precache-manifest.8e7a96a2bedf5923b904591024c692b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553f51b3c2914a51a7a69b56146f8b47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00f55e52be6a60b4ac8e",
+    "url": "/static/css/main.c8c92fe1.chunk.css"
+  },
+  {
+    "revision": "bdf9653056cc657aabb0",
+    "url": "/static/js/2.918af9d7.chunk.js"
+  },
+  {
+    "revision": "00f55e52be6a60b4ac8e",
+    "url": "/static/js/main.3ac2ff62.chunk.js"
+  },
+  {
+    "revision": "92b26f24670b3f286969",
+    "url": "/static/js/runtime-main.084bcad1.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9114c356d7352cc79d500bc987e37114.js"
+  "/precache-manifest.8e7a96a2bedf5923b904591024c692b5.js"
 );
 
 self.addEventListener('message', (event) => {
