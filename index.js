da1ca111fe301402d55bcf8d808a940e76d17df4
Никита Лемешенко <n.lemeshenko@magdv.com>
2019-12-05 09:11:06 +0700

    Heroku added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const resource = "/api/persons";
 app.use(cors());
 app.use(resource, personsRouter);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
